5eefc3f9be077899ba6a69febee7829c0a864ea2
Natsurio <1281580589@qq.com>
2022-08-12 20:50:35 +0800

    调试左侧容器位置

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import '../style/list.css'
 export function List()
 {
-    return (<div className="container-left">
+    return (<div className="container-left" >
         <ul>
         <li><a target="iframe_1">推荐</a></li>
         <li><a target="iframe_1">最新</a></li>
         <li><a target="iframe_1">热榜</a></li>
-        <iframe>
+        <iframe >
 
         </iframe>
         </ul>
