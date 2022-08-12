090b8506f2ed026cf34ba2086d014934aee05d38
Natsurio <1281580589@qq.com>
2022-08-12 16:31:03 +0800

    Create list.tsx

---
+++ src/components/list.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+export function List()
+{
+    return (<div className="container-left">
+        <ul>
+        <li><a target="iframe_1">推荐</a></li>
+        <li><a target="iframe_1">最新</a></li>
+        <li><a target="iframe_1">热榜</a></li>
+        <iframe>
+
+        </iframe>
+        </ul>
+    </div>)
+}
