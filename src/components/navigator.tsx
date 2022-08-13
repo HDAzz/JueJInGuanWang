8af777820416a531cc92ba717944ba55db8aa504
HDAzz <2011653076@qq.com>
2022-08-13 17:16:06 +0800

    none

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -6,8 +6,8 @@ export function Navigator()
                 <a className="logo" href="/">
                     <img alt="稀土掘金" src="//lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/e08da34488b114bd4c665ba2fa520a31.svg"  />
                 </a>
-                <nav className="main-nav">
-                    <ul className="nav-tabs">
+                {/* 上面的导航条 */}
+                    <ul className="main-nav-tabs">
                         <li><span>首页</span></li>
                         <li><span>沸点</span></li>
                         <li><span>课程</span></li>
@@ -17,9 +17,18 @@ export function Navigator()
                         <li><span>APP</span></li>
                         <li><span>插件</span></li>
                     </ul>
-                </nav>
-                <nav className="sub-nav">
-
-                </nav>
+                {/* 下面的导航条 */}
+                    <ul className="sub-nav-tabs">
+                        <li>综合</li>
+                        <li>关注</li>
+                        <li>后端</li>
+                        <li>前端</li>
+                        <li>Android</li>
+                        <li>iOS</li>
+                        <li>人工智能</li>
+                        <li>开发工具</li>
+                        <li>代码人生</li>
+                        <li>阅读</li>
+                    </ul>
             </div>)
 }
