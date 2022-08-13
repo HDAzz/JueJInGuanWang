53fd13aca3cc0f4f3d78d883036fbe921ea9feae
Natsurio <1281580589@qq.com>
2022-08-13 19:15:26 +0800

    8.13

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -4,10 +4,14 @@ export function List()
 {
     return (<div className="container-left" >
         <ul>
-        <li><a >推荐</a></li>
-        <li><a href="/?sort=newest">最新</a></li>
-        <li><a href="/?sort=hottest">热榜</a></li>
-        
+        <div >
+        <li className="up-class"><a href="/">推荐</a></li>
+        <li className="up-class"><a href="/?sort=newest">最新</a></li>
+        <li className="up-class"><a href="/?sort=hottest">热榜</a></li>
+        </div>
+        <div className="passage">
+            <li><a href="https://juejin.cn/pin/7129334097113006116?utm_source=web1&utm_medium=banner&utm_campaign=reading">三分钟阅读挑战</a></li>
+        </div>
         </ul>
     </div>)
 }
