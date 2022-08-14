b295d428184c1fff00e4309f230d42ba8cea44e3
Natsurio <1281580589@qq.com>
2022-08-14 16:16:52 +0800

    8.14

--- src/components/list.tsx
+++ src/components/list.tsx
@@ -6,11 +6,14 @@ export function List()
         <ul>
         <div >
         <li className="up-class"><a href="/">推荐</a></li>
+        <div className="line"></div>
         <li className="up-class"><a href="/?sort=newest">最新</a></li>
+        <div className="line"></div>
         <li className="up-class"><a href="/?sort=hottest">热榜</a></li>
         </div>
         <div className="passage">
-            <li><a href="https://juejin.cn/pin/7129334097113006116?utm_source=web1&utm_medium=banner&utm_campaign=reading">三分钟阅读挑战</a></li>
+            <p className="writer">沸点小助手 | 4天前</p>
+            <li><a href="https://juejin.cn/pin/7129334097113006116?utm_source=web1&utm_medium=banner&utm_campaign=reading">三分钟阅读挑战｜阅读时间不浪费！打开app阅读文章边学习边拿奖品！</a></li>
         </div>
         </ul>
     </div>)
