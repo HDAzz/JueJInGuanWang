be2918cfe5e9d2e161680c1d225d81461d9510c4
HDAzz <2011653076@qq.com>
2022-08-14 18:15:15 +0800

    8.14

--- src/components/navigator.tsx
+++ src/components/navigator.tsx
@@ -9,14 +9,14 @@ export function Navigator()
                     </a>
                     {/* 上面的导航条 */}
                     <ul className="main-nav-tabs">
-                        <li><span>首页</span></li>
-                        <li><span>沸点</span></li>
-                        <li><span>课程</span></li>
-                        <li><span>直播</span></li>
-                        <li><span>活动</span></li>
-                        <li><span>商城</span></li>
-                        <li><span>APP</span></li>
-                        <li><span>插件</span></li>
+                        <li className="active"><a className="link-item">首页</a></li>
+                        <li><a className="link-item">沸点</a></li>
+                        <li><a className="link-item">课程</a></li>
+                        <li><a className="link-item">直播</a></li>
+                        <li><a className="link-item">活动</a></li>
+                        <li><a className="link-item">商城</a></li>
+                        <li><a className="link-item">APP</a></li>
+                        <li><a className="link-item">插件</a></li>
                     </ul>
 
                     <div className="search-box">
@@ -25,10 +25,24 @@ export function Navigator()
                             <img alt="搜索" className="search-icon" src="https://lf3-cdn-tos.bytescm.com/obj/static/xitu_juejin_web/1e8ab9a22f0ddc36349f60b38900d0bd.svg"></img>
                         </div>
                     </div>
+                    <div className="add-group">
+                        <button className="add-btn">创作者中心</button>
+                        <div className="more">
+                            {/* <svg className="unfold-icon"  fill="none" height="12" viewBox="0 0 12 12" width="12" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.45025 4.82383C2.17422 4.49908 2.40501 4 2.83122 4H9.16878C9.59499 4 9.82578 4.49908 9.54975 4.82382L6.38097 8.5518C6.1813 8.7867 5.8187 8.7867 5.61903 8.5518L2.45025 4.82383Z"  fill="white"></path>
+                            </svg> */}
+                            <ul className="more-list">
+                                <li className="item"><span className="icon write-article">写文章</span></li>
+                                <li className="item"><span className="icon issue-points">发沸点</span></li>
+                                <li className="item"><span className="icon create-jcode">写代码</span></li>
+                            </ul>
+                        </div>
+                        
+                    </div>
                 </div>
                 <div className="container-2">
                     {/* 事实证明，文字不等长得用div一个一个装 */}
-                    <a className="nav-item" href="#">
+                    <a className="nav-item active" href="#">
                         <div category-popover-box>综合</div>    
                     </a>
                     <a className="nav-item" href="#">
