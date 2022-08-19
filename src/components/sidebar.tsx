87b13447b4d8b38af1884833347c7c8bd0d0dcff
Natsurio <1281580589@qq.com>
2022-08-19 20:20:49 +0800

    wssb

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import '../style/sidebar.css'
 export function Sidebar()
 {
-    return(<div className="container-right">
-        <span className="title">
+    return(<ul className="container-right">
+        <ul>
+        <div className="firstline">
+          <span className="title">
             下午好！
-        </span>
-        <button className="button">
-            去签到
-        </button>
-    </div>)
+          </span>
+          <span>
+            <button className="button">
+              去签到
+            </button>
+          </span>
+        </div>
+        <div className="wen">
+            点亮你在社区的每一天
+        </div>
+        </ul>
+    </ul>)
 
 }
