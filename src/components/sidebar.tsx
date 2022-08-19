1874489ee52b403164ee499130b787af10badfee
HDAzz <2011653076@qq.com>
2022-08-19 20:47:04 +0800

    8.19

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,22 +2,17 @@ import React from "react";
 import '../style/sidebar.css'
 export function Sidebar()
 {
-    return(<ul className="container-right">
+    return(<div className="container-right">
         <ul>
-        <div className="firstline">
-          <span className="title">
-            下午好！
-          </span>
-          <span>
-            <button className="button">
-              去签到
-            </button>
-          </span>
-        </div>
-        <div className="wen">
+          <div className="firstline">
+            <span className="title">下午好！</span>
+            <span><button className="button">去签到</button></span>
+          </div>
+          <div className="wen">
             点亮你在社区的每一天
-        </div>
+          </div>
         </ul>
-    </ul>)
+    </div>)
 
 }
+
